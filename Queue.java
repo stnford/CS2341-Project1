bc8b9bd7d3377fc75c8c370d64dac6d4941d7fc7
Luke Stanford <122490108+stnford@users.noreply.github.com>
2024-09-25 16:27:46 -0500

    Update Queue.java
    Changed class to take generic types. Completed dequeue() function

--- Queue.java
+++ Queue.java
@@ -2,9 +2,9 @@
 Queue is first in first out so this class will need a front pointer and a rear pointer
 to add (enqueue) to the rear and remove (dequeue) from the front
  */
-public class Queue {
-    private LogNode front;
-    private LogNode rear;
+public class Queue<T> {
+    private LogNode<T> front;
+    private LogNode<T> rear;
 
     //set front and rear to null to create an empty queue.
     public Queue() {
@@ -13,13 +13,13 @@ public class Queue {
     }
 
     //Enqueue to add logs to the rear
-    public void enqueue(LogNode logEntry){
+    public void enqueue(T data){
 
         //create new logNode with the data from the logEntry
-        LogNode newLog = new LogNode(logEntry.data);
+        LogNode<T> newLog = new LogNode<>(data);
 
-        //if the queue is empty set the legNode to the first and last position
-        if(this.front == null){
+        //if the queue is empty set the logNode to the first and last position
+        if(this.rear == null && this.front == null){
             this.front = newLog;
             this.rear = newLog;
             return;
@@ -28,16 +28,26 @@ public class Queue {
             this.rear.next = newLog;    //.next to save its position at the end of the queue
             this.rear = newLog;     //.rear to actually set it as the rear
         }
-
     }
 
-    public void dequeue(){
+    public T dequeue(){
+        //if the queue is empty return null
         if(this.front == null){
             System.out.println("Queue is empty");
+            return null;
         }
-        else{
+        else {
+            T removal = this.front.data; //if the queue isnt empty get the data from the front node
+            this.front = this.front.next; //set the first node to the next node
+            if(this.front == null){     //if the queue is now empty set the rear to null
+                this.rear = null;
+            }
+            return removal; //return the removed data
+        }
+    }
 
-        }
+    public boolean isEmpty(){
+        return this.front == null;
     }
 
     public void printLast(){
@@ -54,7 +64,7 @@ public class Queue {
             System.out.println("Queue is empty");
         }
         else{
-            LogNode current = front;
+            LogNode<T> current = front;
             while(current != null){
                 System.out.println(current.data);
                 current = current.next;
