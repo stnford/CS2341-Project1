b0e79c0b18441038f7285ccbbb583dc5dba1d474
Luke Stanford <122490108+stnford@users.noreply.github.com>
2024-09-26 11:28:10 -0500

    Update Stack.java
    Added a print function

--- Stack.java
+++ Stack.java
@@ -1,59 +1,39 @@
+public class Stack<T> {
+    private LogNode<T> top;
 
-    public class Stack<Type> {
-        private Node first, last;
-
-        private class Node {
-            Type item;
-            Node next;
-        }
-
-        // Constructor
-        public Stack() {
-            first = null;
-            last = null;
-        }
-
-        public boolean isEmpty() {
-            return first == null;
-        }
-
-        // adds an item to the front
-        public void push(Type item) {
-            if (isEmpty()) {
-
-                last = new Node();
-                last.item = item;
-                last.next = null;
-                first = last;
-            } else {
-                Node oldFirst = first;
-                first = new Node();
-                first.item = item;
-                first.next = oldFirst;
-            }
-        }
-
-        // removes item from the front
-        public Type pop() {
-            if (isEmpty()) {
-                System.out.println("Stack is empty");
-                return null;
-            }
-            Type item = first.item;
-            first = first.next;
-
-            if (isEmpty()) {
-                last = null; // Reset last if the list becomes empty
-            }
-            return item;
-        }
-        public void printStack() {
-            Node current = first;
-            while (current != null) {
-                System.out.print(current.item + " ");
-                current = current.next; // goes to the next node
-            }
-            System.out.println();
-        }
+    // Constructor
+    public Stack() {
+        this.top = null;
     }
 
+    public boolean isEmpty() {
+        return top == null;     //isEmpty() will return 'true' if the top is empty.
+    }
+
+    // adds an item to the front
+    public void push(T item) {
+        //EVEN IF the stack is empty we can add our item to the top of the stack
+        LogNode<T> newNode = new LogNode<T>(item);
+        newNode.next = top;
+        top = newNode;
+    }
+
+    public T pop(){
+        if(top == null){
+            return null;    //if the top is empty the whole stack is empty, return null
+        }
+        T item = top.data;
+        top = top.next;
+        return item;
+    }
+
+    public void print(){
+        if(top == null){
+            System.out.println("Stack is empty");
+        }
+        else{
+            System.out.println(top.data);
+            top = top.next;
+        }
+    }
+}
