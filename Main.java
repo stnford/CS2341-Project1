5c2190cd8d3d7349fad54ab6ea05d072a288e3f1
LAPTOP-AD4PHL6K\cammc <leximc04@gmail.com>
2024-09-25 23:16:45 -0500

    spelling changes and merging Lukes changes into main

--- Main.java
+++ Main.java
@@ -4,11 +4,34 @@ public class Main {
         Queue queue = new Queue();
 
         while(!StdIn.isEmpty()){
-            LogNode newNode = new LogNode(StdIn.readLine());
-            queue.enqueue(newNode);
+            LogNode<String> newNode = new LogNode<String>(StdIn.readLine());
+            queue.enqueue(newNode.data);
         }
 
         queue.printQueue();
+
+        while (!queue.isEmpty()) {
+            String item = queue.dequeue(); // dequeue needs to be fixed
+            if(item != null && item.contains("ERROR")){
+                stack.push(item);
+                errorCount++;
+            }
+            else if (item!= null && item.contains("WARN")){
+                warnCount++;
+                if(item.contains("Memory")){
+                    warnMemCount++;
+                }
+            }
+            else if(item!= null && item.contains("INFO")){
+                infoCount++;
+            }
+        }
+
+        System.out.println("CSV Analysis: ");
+        System.out.println("Error count: " + errorCount);
+        System.out.println("All Warnings log count: " + warnCount);
+        System.out.println("Memory Warnings log count: " + warnMemCount);
+        System.out.println("Info log count: " + infoCount);
 
     }
 }
