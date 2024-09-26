c9865c2e45893d316b6f6684483fc5bfc8ce8a9b
LAPTOP-AD4PHL6K\cammc <leximc04@gmail.com>
2024-09-25 23:09:20 -0500

    Updated LogNode to correct generic implementation

--- LogNode.java
+++ LogNode.java
@@ -1,11 +1,11 @@
 /*
 Since the project uses linked list form this class will be called to create a node for each log entry
  */
-public class LogNode {
-    String data;
+public class LogNode<T> {
+    T data;
     LogNode next;
 
-    public LogNode(String data){
+    public LogNode(T data){
         this.data = data;
         this.next = null;
     }
